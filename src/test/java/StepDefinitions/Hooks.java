5448fff6e8760062da59bb37442905a9b786203b
Onizuka Eikichi <rukiaaa@o2.pl>
2025-12-06 17:19:34 +0100

    Dodanie zrzutu ekranu do raportu cucumbera.

--- src/test/java/StepDefinitions/Hooks.java
+++ src/test/java/StepDefinitions/Hooks.java
@@ -2,6 +2,10 @@ package StepDefinitions;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 import service.driver.DriverFactory;
 
 
@@ -14,8 +18,20 @@ public class Hooks {
 	}
 
 	@After
-	public void tearDown() {
-		// Sprzątanie po każdym scenariuszu
+	public void tearDown(Scenario scenario) {
+
+		// Sprawdzamy, czy scenariusz zakończył się błędem
+		if (scenario.isFailed()) {
+			WebDriver driver = DriverFactory.getDriver();
+			if (driver != null) {
+				// Robimy zdjęcie
+				byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+
+				// Dołączamy zdjęcie do raportu Cucumbera
+				scenario.attach(screenshot, "image/png", "Zrzut ekranu błędu");
+			}
+		}
+		// Zamykamy przeglądarkę niezależnie od wyniku
 		DriverFactory.quitDriver();
 	}
 }
